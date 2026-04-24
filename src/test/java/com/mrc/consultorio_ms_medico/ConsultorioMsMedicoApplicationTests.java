7a67f60a16d73f985be0899c24309ee223c8add1
Mauricio Carvalho <mauriciocarvalhoac@gmail.com>
2026-04-24 11:16:32 -0300

    Configurando o application properties para dev e tqs

--- src/test/java/com/mrc/consultorio_ms_medico/ConsultorioMsMedicoApplicationTests.java
+++ src/test/java/com/mrc/consultorio_ms_medico/ConsultorioMsMedicoApplicationTests.java
@@ -1,13 +1,13 @@
 package com.mrc.consultorio_ms_medico;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
-@SpringBootTest
+@WebMvcTest(ConsultorioMsMedicoApplication.class)
 class ConsultorioMsMedicoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
